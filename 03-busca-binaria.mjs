9ccf0789eebb14a5604c6da4ed4157f99586f4c3
gabriel-terencio <gabrielterencio77@gmail.com>
2021-08-24 00:44:49 +0000

    (23/08) Início da busca binária

---
+++ 03-busca-binaria.mjs
@@ -0,0 +1,68 @@
+/*
+    * BUSCA BINÁRIA
+
+    * Requer um conjunto de dados ORDENADO.
+    * Atua dividindo o vetor sucessivamente em metades aproximadas,
+    * até que o valor de busca seja localizado, ou que o ponteiro
+    * de fim acabe antes do ponteiro de início. Essa última situação
+    * índica que o valor de busca não existe no conjunto.
+*/
+
+let comps
+
+function buscaBinaria(vetor, valorBusca) {
+    comps = 0
+    let ini = 0
+    let fim = vetor.length - 1
+
+    while(fim >= ini) {
+        comps++
+
+        // Math.floor() retira as casas decimais de um número
+        let meio = Math.floor((ini + fim) / 2)
+
+        // Se o valor de busca for igual ao valor do veto
+        // na posição do meio, encontramos o que procuramos
+        // e retornamos a posição onde encontramos
+        if(valorBusca === vetor[meio]) {
+            comps++
+            return meio
+        }
+        // Senão, se o valor de busca for maior que o valor
+        // do meio do vetor, descartamos a metade esquerda
+        // do vetor trazendo o ponteiro ini para meio + 1
+        else if(valorBusca > vetor[meio]) {
+            comps += 2
+            ini = meio + 1
+        }
+        
+        // Por fim, caso o valor de busca seja menor que o 
+        // valor do meio do vetor, descartamos a metade direita
+        // do vetor trazendo o ponteiro fim para meio - 1
+        else {
+            comps += 2
+            fim = meio - 1
+        } 
+
+    }
+    // Se chegamos até aqui, significa que fim < ini e, portanto,
+    // o valor de busca não existe no vetor. Para indicar isso, 
+    // retornamos o valor convencional - 1.
+    return -1
+}
+
+let nums = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99]
+
+console.log('Posiçao de 33:', buscaBinaria(nums, 33), 'comps:', comps)
+console.log('Posiçao de 77:', buscaBinaria(nums, 77), 'comps:', comps)
+console.log('Posiçao de 81:', buscaBinaria(nums, 81), 'comps:', comps)
+
+/*********************************************************************************/
+
+import { nomes } from './Data/vetor-nomes.mjs'
+
+console.log(`Posição de GABRIEL: ${buscaBinaria(nomes, 'GABRIEL')}, comps: ${comps}`)
+console.log(`Posição de ADAMASTOR: ${buscaBinaria(nomes, 'ADAMASTOR')}, comps: ${comps}`)
+console.log(`Posição de ZULEIDE: ${buscaBinaria(nomes, 'ZULEIDE')}, comps: ${comps}`)
+console.log(`Posição de INSTAGRAMILDA: ${buscaBinaria(nomes, 'INSTAGRAMILDA')}, comps: ${comps}`)
+console.log(`Posição de JERDERSON: ${buscaBinaria(nomes, 'JERDERSON')}, comps: ${comps}`)
---
+++ 04-busca-binaria-obj.mjs
@@ -0,0 +1,52 @@
+let comps
+
+function buscaBinaria(vetor, valorBusca, fnComp) {
+    comps = 0
+    let ini = 0
+    let fim = vetor.length - 1
+
+    while(fim >= ini) {
+        comps++
+
+        // Math.floor() retira as casas decimais de um número
+        let meio = Math.floor((ini + fim) / 2)
+
+        switch(fnComp(vetor[meio])) {
+            case 0:    // Encontrado o valor de busca
+                return meio
+            
+            case 1:    // Valor busca > valor do meio do vetor
+                ini = meio + 1
+                break
+
+            default:   // -1, valor de busca < valor do meio do vetor
+                fim = meio - 1
+
+        }
+
+    }
+    // Se chegamos até aqui, significa que fim < ini e, portanto,
+    // o valor de busca não existe no vetor. Para indicar isso, 
+    // retornamos o valor convencional - 1.
+    return -1
+}
+
+/*
+    Na busca binária, a comparação entre o valor de busca e o valor
+    que está no meio do vetor tem três possibilidades:
+    1º -> ambos os valores são IGUAIS
+    2º -> o valor de busca é MAIOR que o valor do meio do vetor
+    3º -> o valor de busca é MENOR que o valor do meio do vetor
+
+    Para usar a busca binária em um VETOR DE OBJETOS, precisamos
+    transferir a comparação para uma função externa que retorne um
+    dos três valores:
+    0 -> caso ambos os valores sejam IGUAIS
+    1 -> caso o valor de busca seja MAIOR que o valor do meio do vetor
+    -1 -> caso o valor de busca seja MENOR que o valor do meio do vetor
+*/
+function comparar(valorMeio, valorBusca = 'GABRIEL') {
+    if(valorBusca === valorMeio.first_name) return 0
+    else if(valorBusca > valorMeio.first_name) return 1
+    else return -1
+}
